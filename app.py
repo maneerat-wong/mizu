64a60c5b16605cc13a200853bbeb5ac1d4a6a474
Maneerat Wongjaroenporn <jew@Maneerats-Air.lan>
2024-10-08 18:35:54 -0700

    update the timezone in the page

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ data_date = eastern_now.date() - datetime.timedelta(days=1)
 SEASONAL_YEAR = data_date.year + 1 if data_date.month >= 6 else data_date.year
 
 st.title(f"Water Allocation Prediction for {SEASONAL_YEAR} summer season")
-st.text(f'Today is {datetime.date.today()}')
+st.text(f'Today is {eastern_now.date()}')
 clicked = st.button("Run the model")
 if clicked:
     with st.spinner(f'Running the model for {data_date}... This may take a while'):
