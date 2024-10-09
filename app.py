a098992e783b6bdba933ed772ccd8c30d8ce8f0a
Maneerat Wongjaroenporn <jew@Maneerats-Air.lan>
2024-10-08 18:20:29 -0700

    test date'

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import datetime
+from pytz import timezone
 import time
 from model_prediction.predict_allocation import train_daily_model, predict
 
@@ -7,7 +8,7 @@ data_date = datetime.date.today() - datetime.timedelta(days=1)
 SEASONAL_YEAR = data_date.year + 1 if data_date.month >= 6 else data_date.year
 
 st.title(f"Water Allocation Prediction for {SEASONAL_YEAR} summer season")
-
+st.text(f'Today is {datetime.date.today()}')
 clicked = st.button("Run the model")
 if clicked:
     with st.spinner(f'Running the model for {data_date}... This may take a while'):
